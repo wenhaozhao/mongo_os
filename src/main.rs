e85fe9da9d6e5f96d8747c1ac74b838bff94c8bb
wenhaozhao <wenhaozhao@sohu-inc.com>
2023-07-03 22:05:28 +0800

    add vga_buffer.rs

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,21 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
+
 #[panic_handler]
-fn panic(_info: &PanicInfo) ->! {
+fn panic(info: &PanicInfo) ->! {
+    println!("{}", info);
     loop {
     }
 }
 
-static TEXT_WELCOME: &[u8]=b"Welcome to MonogoOS";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in TEXT_WELCOME.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Welcome to MonogoOS");
     loop {}
 }
 
